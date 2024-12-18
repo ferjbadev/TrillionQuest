48ae395e0b80b77b227c7b8199aaac68e604c9ec
Fernando Barrera <barrerafernando54@gmail.com>
2024-12-18 19:01:24 -0400

    Narbar

---
+++ TrillionQuest/src/App.jsx
@@ -0,0 +1,16 @@
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+
+  )
+}
+
+export default App
